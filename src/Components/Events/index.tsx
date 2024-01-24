5f91af14d1f9678df74c10fec7c4160bcfb34a59
quanict <hongquan2712@gmail.com>
2024-01-24 17:15:49 +0700

    update 2024

--- src/Components/Events/index.tsx
+++ src/Components/Events/index.tsx
@@ -1,6 +1,7 @@
 import {events as familyEvents} from './Family';
 import {events as holidayEvents} from './Holiday';
 import {events as gmo2023} from './Runsystem2023';
+import {events as gmo2024} from './Runsystem2024';
 
-const events = [...familyEvents, ...holidayEvents, ...gmo2023];
+const events = [...familyEvents, ...holidayEvents, ...gmo2023, ...gmo2024];
 export default events;
